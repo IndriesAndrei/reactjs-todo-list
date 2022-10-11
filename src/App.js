ecb6e12fb08904574a7a7ce7b36c053e7f21a5b3
Andrei Indries <andrei.indries@wesrom.com>
2022-10-11 12:59:35 +0300

    ReactJs 18 TodoList App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import TodoList from "./TodoList";
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
+  // todos = actual state, setTodos -> set new state
+  const [todos, setTodos] = useState([]);
+
+  // accessing the name from the input field
+  // useRef allows us to reference elements inside the HTML
+  const todoNameRef = useRef();
+
+  // call this only once
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos( prevTodos => [...prevTodos, ...storedTodos] );
+  }, [])
+
+  // call this if the state of todos changes
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null;
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete);
+    setTodos(newTodos);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* pass props to TodList component
+      passing todos variable to the todos prop */}
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Completed</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
   );
 }
 
